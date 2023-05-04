5e83d62d9e5f26a3c5c28ca06ad0cf6068909539
Nikita <911562726@qq.com>
2023-05-04 16:12:15 +0800

    正确的main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import controller.Server;
+import controller.User;
+import view.MainGameFrame;
+import music.MusicThread;
+
+import javax.swing.*;
+import java.net.URL;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SwingUtilities.invokeLater(() -> {
+            Server server = new Server();
+            User user = new User();
+            MainGameFrame mainFrame = new MainGameFrame(800, 1000, server, user);
+            mainFrame.setVisible(true);
+        });
+
+        URL musicURL = Main.class.getResource("/music/bgMusic.wav");
+
+
+        // 创建音乐线程实例
+        MusicThread musicThread = new MusicThread(musicURL, true);
+
+        // 创建线程并启动
+        Thread music = new Thread(musicThread);
+        music.start();
+
+        // 调整音量
+        musicThread.setVolume(0.5f); // 设置音量为一半
+        float volume = musicThread.getVolume(); // 获取当前音量
+        System.out.println("volume: " + volume);
     }
+    // TODO: 1. music and sound effect 2. platform 3. AI 4. network
 }
